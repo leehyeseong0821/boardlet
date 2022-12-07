a2e01ccf329b6c4c4c858ea781143e5027384a24
leehyeseong0821 <lhs5567@naver.com>
2022-12-07 14:55:41 +0900

    개인 프로젝트 게시판 구현

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -3,10 +3,16 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -23,17 +29,40 @@ public class BoardController {
 
         return "boardwrite";
     }
-    @PostMapping("/board/writepro") //폼 액션이랑 같아야함.
-    public String boardWritePro(Board board){
-
+    @PostMapping("/board/list") //폼 액션이랑 같아야함.
+    public String boardWritePro(Board board, Model model) {
         boardService.write(board);
+        model.addAttribute("list",boardService.boardList(Pageable.unpaged()));
+        model.addAttribute("message","글 작성이 완료 되었습니다.");
+        model.addAttribute("searchUrl","/board/list");
 
-        return  "";
+
+        return  "message";
     }
     @GetMapping("/board/list")
-    public String boardList(Model model){
+    public String boardList(Model model,
+                            @PageableDefault(page = 0, size = 10, sort ="id", direction = Sort.Direction.DESC)Pageable pageable,
+                            String searchKeyword){
 
-        model.addAttribute("list",boardService.boardList());
+        Page<Board> list = null;
+
+        if(searchKeyword == null){
+            list = boardService.boardList(pageable);
+        }else{
+            list = boardService.boardSearchList(searchKeyword, pageable);
+        }
+
+
+
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+
+        model.addAttribute("list",list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
 
         return "boardlist";
     }
@@ -42,5 +71,26 @@ public class BoardController {
 
         model.addAttribute("board",boardService.boardView(id));
         return "boardview";
+    }
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id,Model mode){
+         mode.addAttribute("board",boardService.boardView(id));
+        return "boardmodify";
+    }
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id,Board board) {
+
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setName(board.getName());
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+        boardService.write(boardTemp);
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -3,9 +3,14 @@ package com.study.board.service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -13,19 +18,33 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     //글 작성
-    public void write(Board board){
+    public void write(Board board) {
+
 
         boardRepository.save(board);
 
     }
     //게시글 리스트 처리
-    public List<Board> boardList(){
-        return boardRepository.findAll(); //list에 board란 클래스를 반환
+    public Page<Board> boardList(Pageable pageable){
+        return boardRepository.findAll(pageable); //list에 board란 클래스를 반환
     }
+
+    public Page<Board> boardSearchList(String searchKeyword,Pageable pageable){
+
+        return boardRepository.findByTitleContaining(searchKeyword,pageable);
+    }
+
+
     // 특정 게시글 불러오기
     public Board boardView(Integer id) {
 
         return boardRepository.findById(id).get();
+
+    }
+    // 특정 게시글 삭제
+
+    public void boardDelete(Integer id){
+        boardRepository.deleteById(id);
 
     }
 }
